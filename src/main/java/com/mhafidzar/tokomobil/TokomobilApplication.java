406467c0af6fae950fb16813ce7da1b05a8c8887
natar <natar@MHAFIDZAR>
2019-08-06 23:44:57 +0700

    Init Project

---
+++ src/main/java/com/mhafidzar/tokomobil/TokomobilApplication.java
@@ -0,0 +1,13 @@
+package com.mhafidzar.tokomobil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokomobilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokomobilApplication.class, args);
+	}
+
+}
